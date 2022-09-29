02da68368b201590b626b311dd02c017c58d43b6
Matheus Feres Turcheti <71662383+matheus-ft@users.noreply.github.com>
2022-09-29 09:02:55 -0300

    Fix notebook keybindings

--- after/plugin/jupyter.lua
+++ after/plugin/jupyter.lua
@@ -1,12 +1,13 @@
 vim.g.jupytext_fmt = 'py:percent'
 
 local noremap = { noremap = true }
-local expr = { noremap = true, expr = true }
+local expr = { expr = true, noremap = true }
 vim.keymap.set('n', '<leader>v', 'nvim_exec("MagmaEvaluateOperator", v:true)', expr)
-vim.keymap.set({'v', 'x'}, '<S-CR>', '<cmd>MagmaEvaluateVisual<CR>', noremap)
-vim.keymap.set({'n', 'i'}, '<S-CR>', '<Esc>vip<cmd>MagmaEvaluateLine<CR><Esc>', noremap)
-vim.keymap.set({'n', 'i'}, '<C-CR>', '<cmd>MagmaEvaluateLine<CR>', noremap)
-vim.keymap.set('n', '<leader>j', '<cmd>MagmaShowOutput<CR>', noremap)
+vim.keymap.set({'v', 'x'}, '<S-CR>', ':MagmaEvaluateVisual<CR>', noremap)
+vim.keymap.set({'n', 'i'}, '<S-CR>', '<Esc>vip:<BS><BS><BS><BS><BS>MagmaEvaluateVisual<CR><Esc>', noremap)
+vim.keymap.set({'n', 'i'}, '<C-CR>', ':MagmaEvaluateLine<CR>', noremap)
+vim.keymap.set('n', '<leader>j', ':MagmaShowOutput<CR>', noremap)
+-- vim.keymap.set('n', '<leader>?', ':noautocmd MagmaEnterOutput<CR>', noremap)
 
 vim.g.magma_automatically_open_output = false
 vim.g.magma_image_provider = "ueberzug"
