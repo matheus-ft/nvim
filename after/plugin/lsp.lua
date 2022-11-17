a54041943b913392a3ae11bea629b722ce537dfc
Matheus Feres Turcheti <71662383+matheus-ft@users.noreply.github.com>
2022-11-16 21:32:10 -0300

    Improve looks and functionality

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,13 +5,38 @@ vim.opt.completeopt:append { 'menu', 'menuone', 'noselect' }
 
 local cmp = require('cmp')
 
+-- borrowed from NvChad
+local function border(hl_name)
+  return {
+    { "╭", hl_name },
+    { "─", hl_name },
+    { "╮", hl_name },
+    { "│", hl_name },
+    { "╯", hl_name },
+    { "─", hl_name },
+    { "╰", hl_name },
+    { "│", hl_name },
+  }
+end
+
 cmp.setup({
+  window = {
+    completion = {
+      border = border "CmpBorder",
+      winhighlight = "Normal:CmpPmenu,CursorLine:PmenuSel,Search:None",
+    },
+    documentation = {
+      border = border "CmpDocBorder",
+    },
+  },
+
   mapping = {
-    ["<C-n>"]   = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert }, -- default vim behavior
-    ["<C-p>"]   = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert }, -- default vim behavior
-    ["<Tab>"]   = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-    ["<S-Tab>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-    ['<CR>']    = cmp.mapping.confirm({ select = false }), -- accept the explicitly selected item
+    ["<C-n>"]     = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }), -- default vim behavior
+    ["<C-p>"]     = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }), -- default vim behavior
+    ["<Tab>"]     = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+    ["<S-Tab>"]   = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+    ['<CR>']      = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }), -- accept the explicitly selected item
+    ["<C-Space>"] = cmp.mapping.complete(),
 
     ["<c-y>"] = cmp.mapping(-- borrowed from Teej
       cmp.mapping.confirm {
@@ -22,12 +47,13 @@ cmp.setup({
     ),
 
     ["<c-e>"] = cmp.mapping.abort(),
-    ["<c-b>"] = cmp.mapping.scroll_docs(-4),
-    ["<c-f>"] = cmp.mapping.scroll_docs(4),
+    ["<c-u>"] = cmp.mapping.scroll_docs(-4),
+    ["<c-d>"] = cmp.mapping.scroll_docs(4),
   },
 
   -- sources of autocompletion (in order of priority)
   sources = {
+    { name = 'nvim_lua' }, -- only works for lua
     { name = "nvim_lsp" },
     { name = "path" },
     { name = "luasnip" },
@@ -47,13 +73,8 @@ cmp.setup({
   },
 })
 
--- Set configuration for specific filetype.
-cmp.setup.filetype('lua', {
-  sources = {
-    { name = 'nvim_lua' },
-    { name = 'buffer' },
-  }
-})
+-- Set configuration for specific filetype
+-- cmp.setup.filetype()
 
 -- integration with LSP
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
@@ -70,7 +91,7 @@ local on_attach = function(client, bufnr)
   -- Mappings
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
   vim.keymap.set('n', '<A-n>', '<cmd>lua require"illuminate".next_reference{wrap=true}<cr>', bufopts)
-  vim.keymap.set('n', '<A-p>', '<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>', bufopts)
+  vim.keymap.set('n', '<A-N>', '<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>', bufopts)
   vim.keymap.set('n', '<S-k>', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
@@ -79,8 +100,8 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
   vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', '<leader>f', vim.lsp.buf.formatting, bufopts)
-  vim.keymap.set('n', '<C><S-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, bufopts)
+  vim.keymap.set('n', '<C><S-k>', vim.lsp.buf.signature_help, bufopts) -- never works
 
   vim.keymap.set('n', '<leader>nd', vim.diagnostic.goto_next, bufopts)
   vim.keymap.set('n', '<leader>Nd', vim.diagnostic.goto_prev, bufopts)
@@ -90,32 +111,50 @@ end
 -- Languages settings
 ---------------------------------------------------------------------------------------
 require('nvim-treesitter.configs').setup({
-  ensure_installed = { 'lua', 'bash', 'python' },
-  highlight = { enable = true },
+  ensure_installed = { 'lua', 'bash', 'python', "markdown" },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false
+  },
 })
+
+local servers = { 'sumneko_lua', 'bashls', 'pyright', 'marksman' }
 
 -- this have to be set up before any servers are set
 require("mason-lspconfig").setup({
-  ensure_installed = { 'sumneko_lua', 'bashls', 'pyright', 'marksman' },
+  ensure_installed = servers,
   automatic_installtion = true,
 })
 
-require('lspconfig')['sumneko_lua'].setup({
+for _, server in ipairs(servers) do
+  require("lspconfig")[server].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+  })
+end
+
+-- borrowed from NvChad
+require("lspconfig")["sumneko_lua"].setup({
   capabilities = capabilities,
   on_attach = on_attach,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+          [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+        },
+        maxPreload = 100000,
+        preloadFileSize = 10000,
+      },
+    },
+  },
 })
 
-require('lspconfig')['bashls'].setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
-})
-
-require('lspconfig')['pyright'].setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
-})
-
--- little hack to have formatting with pyright
+-- little hack to have personalized formatting
 require('lspconfig')['diagnosticls'].setup({
   filetypes = { "python" },
   init_options = {
@@ -124,14 +163,9 @@ require('lspconfig')['diagnosticls'].setup({
       docformatter = { command = "docformatter", args = { "-" } },
     },
     formatFiletypes = {
-      python = { "black", "docformatter" }
+      python = { "black", "docformatter" } -- pyright does not have its own formatter
     }
   }
-})
-
-require('lspconfig')['marksman'].setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
 })
 
 ---------------------------------------------------------------------------------------
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,23 +1,66 @@
+local telescope = require("telescope")
 local noremap = { noremap = true }
 local key = vim.keymap.set
 local actions = require('telescope.actions')
 
 key('n', '<leader>pf', ':lua require("telescope.builtin").find_files()<CR>', noremap)
 key('n', '<leader>ps', ':lua require("telescope.builtin").live_grep()<CR>', noremap)
-key('n', '<leader>pu', ':lua require("telescope.builtin").grep_string { search = vim.fn.expand("<cword>") }<CR>', noremap)
-key('n', '<leader><Tab>', ':lua require("telescope.builtin").buffers()<CR>', noremap)
-key('n', '<leader>a', ':lua require("telescope.builtin").help_tags()<CR>', noremap)
+key('n', '<leader>pb', ':lua require("telescope.builtin").buffers()<CR>', noremap)
+key('n', '<leader>h', ':lua require("telescope.builtin").help_tags()<CR>', noremap)
 key('n', '<leader>pd', ':Telescope diagnostics<CR>', noremap)
 
-require('telescope').setup({
-  defaults = {
-    initial_mode = 'insert',
-    mappings = {
-      i = {
-        ['<esc>'] = actions.close
-      },
-    },
-  }
-})
 
-require('telescope').load_extension('fzy_native')
+-- borrowed from NvChad
+local options = {
+  defaults = {
+    vimgrep_arguments = {
+      "rg",
+      "-L",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+    },
+    prompt_prefix = "   ",
+    selection_caret = "> ",
+    initial_mode = "insert",
+    selection_strategy = "reset",
+    sorting_strategy = "ascending",
+    layout_strategy = "horizontal",
+    layout_config = {
+      horizontal = {
+        prompt_position = "top",
+        preview_width = 0.55,
+        results_width = 0.8,
+      },
+      vertical = {
+        mirror = false,
+      },
+      width = 0.90,
+      height = 0.90,
+      preview_cutoff = 120,
+    },
+    winblend = 0,
+    border = {},
+    borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+    color_devicons = true,
+    set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+    path_display = { "truncate" },
+    mappings = {
+      n = { ["q"] = actions.close },
+    },
+  },
+
+  extensions_list = { "fzy_native" },
+}
+
+telescope.setup(options)
+
+-- load extensions
+pcall(function()
+  for _, ext in ipairs(options.extensions_list) do
+    telescope.load_extension(ext)
+  end
+end)
