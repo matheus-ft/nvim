a5b7d0fdf070c37c358e432902c020a9c47fde13
Matheus Feres Turcheti <71662383+matheus-ft@users.noreply.github.com>
2022-12-22 17:10:52 -0300

    Refactor and fix typos

--- lua/matheus/init.lua
+++ lua/matheus/init.lua
@@ -1,13 +1,10 @@
 M = {}
 
-M.noremap = function(mode, lhs, rhs, desc, opts)
-  opts = opts or {}
+local keybinding = function(mode, lhs, rhs, desc, opts)
   opts.buffer = opts.buffer or nil -- default is global
   opts.silent = opts.silent or false
   opts.nowait = opts.nowait or false
   opts.expr = opts.expr or false
-  opts.remap = true
-  desc = desc or opts.desc or ""
   local ok, which_key = pcall(require, "which_key")
   if ok then
     opts.mode = mode
@@ -18,22 +15,18 @@ M.noremap = function(mode, lhs, rhs, desc, opts)
   end
 end
 
+M.noremap = function(mode, lhs, rhs, desc, opts)
+  opts = opts or {}
+  opts.remap = false
+  desc = desc or opts.desc or ""
+  keybinding(mode, lhs, rhs, desc, opts)
+end
+
 M.map = function(mode, lhs, rhs, desc, opts)
   opts = opts or {}
-  opts.buffer = opts.buffer or nil -- default is global
-  opts.silent = opts.silent or false
-  opts.nowait = opts.nowait or false
-  opts.expr = opts.expr or false
   opts.remap = true
   desc = desc or opts.desc or ""
-  local ok, which_key = pcall(require, "which_key")
-  if ok then
-    opts.mode = mode
-    which_key.register({ [lhs] = { rhs, desc } }, opts)
-  else
-    opts.desc = desc
-    vim.keymap.set(mode, lhs, rhs, opts)
-  end
+  keybinding(mode, lhs, rhs, desc, opts)
 end
 
 return M
