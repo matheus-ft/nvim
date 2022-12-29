2ac638756dd1b3f5a54afa6ad7324b996d536238
Matheus Feres Turcheti <71662383+matheus-ft@users.noreply.github.com>
2022-12-29 19:44:10 -0300

    Add some markdown settings

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,4 +1,15 @@
+local noremap = require('matheus').noremap
+
 vim.opt_local.textwidth = 120
 vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
 vim.opt_local.shiftwidth = 2
+
+noremap('i', '<C-i>', '**<Left>', 'Italics')
+noremap('i', '<C-b>', '****<Left><Left>', 'Bold')
+
+local ok, autopairs = pcall(require, 'nvim-autopairs')
+if ok then
+  local Rule = require('nvim-autopairs.rule')
+  autopairs.add_rules({ Rule('*', '*', 'markdown') })
+end
